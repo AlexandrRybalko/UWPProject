a6e8f63c339c9f4fa8f526866697b9624e8b37a7
Aleksandr Rybalko <rybalkosanya20@gmail.com>
2021-04-29 17:57:48 +0300

    added writing to a file

--- UWPProject/CameraPage.xaml.cs
+++ UWPProject/CameraPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//using libVLCX;
+using System;
+using System.IO;
 using UWPProject.Entities;
 using UWPProject.ViewModels;
 using Windows.ApplicationModel.Resources;
@@ -7,6 +9,9 @@ using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using LibVLCSharp.Shared;
+using Windows.UI.Core;
+using Xabe.FFmpeg;
 
 namespace UWPProject
 {
@@ -14,6 +19,9 @@ namespace UWPProject
     {
         private FFmpegInterop.FFmpegInteropMSS ffmpeg;
         private ResourceLoader resourceLoader;
+        private bool isRecording;
+        private MediaPlayer mediaPlayer;
+        private LibVLC libVLC;
 
         public CameraViewModel CameraViewModel { get; set; }
         public ButtonCommand GoBackCommand { get; set; }
@@ -26,6 +34,7 @@ namespace UWPProject
 
             string language = ApplicationData.Current.LocalSettings.Values["Language"] as string;
             resourceLoader = ResourceLoader.GetForCurrentView(language);
+            isRecording = false;
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -63,7 +72,7 @@ namespace UWPProject
 
                 CameraViewModel.AddToRecent();
             }
-            catch(System.Runtime.InteropServices.COMException exception)
+            catch (System.Runtime.InteropServices.COMException exception)
             {
                 ShowContentDialog(exception.Message);
                 GoBack();
@@ -100,6 +109,44 @@ namespace UWPProject
             dialog.CloseButtonText = "OK";
 
             await dialog.ShowAsync();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var currentDirectory = ApplicationData.Current.LocalFolder.Path;
+            var destination = Path.Combine(currentDirectory, $"{this.CameraViewModel.City}_{this.CameraViewModel.Country}.mp4");
+            isRecording = !isRecording;
+            if (isRecording)
+            {
+                // Record in a file "record.ts" located in the bin folder next to the app
+                
+                // Load native libvlc library
+                Core.Initialize();
+
+                libVLC = new LibVLC();
+                mediaPlayer = new MediaPlayer(libVLC);
+
+                libVLC.Log += (s, v) => Console.WriteLine($"[{v.Level}] {v.Module}:{v.Message}");
+
+                // Create new media with HLS link
+                using (var media = new Media(libVLC, new Uri(this.CameraViewModel.RtspAddress),
+                    // Define stream output options.
+                    // In this case stream to a file with the given path and play locally the stream while streaming it.
+                    ":sout=#file{dst=" + destination + "}",
+                    ":sout-keep"))
+                {
+                    // Start recording
+                    this.mediaPlayer.Play(media);
+                }
+            }
+            else
+            {
+                this.mediaPlayer.Stop();
+                mediaPlayer.Dispose();
+                libVLC.Dispose();
+
+                //await Conversion.Convert(destination, currentDirectory + @"\file.mp4").Start();
+            }            
         }
     }
 }
